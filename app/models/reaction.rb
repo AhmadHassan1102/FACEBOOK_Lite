65223a6092557e82c2ef6a37b5fd8ea48e83a2ec
Ibtisam ur rehman <ibtisam9220@gmail.com>
2022-08-07 20:46:59 -0700

    Testing Commit

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,5 +1,5 @@
 class Reaction < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :user_id, :post_id, uniqueness: {"Cannot Like a post Twice"}
+  validates :user_id, :post_id, uniqueness: {Message: "Cannot Like a post Twice" }
 end
