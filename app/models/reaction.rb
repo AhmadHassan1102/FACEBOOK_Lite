58f03c136bfa245895076ad9d5a75c560fc4c9db
Ibtisam ur rehman <ibtisam9220@gmail.com>
2022-08-08 15:03:03 -0700

    Corrected 'reaction' validation

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,5 +1,5 @@
 class Reaction < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :user_id, :post_id, uniqueness: {Message: "Cannot Like a post Twice" }
+  validates :user_id, uniqueness: {scope: :post_id ,Message: "Cannot Like a post Twice" }
 end
