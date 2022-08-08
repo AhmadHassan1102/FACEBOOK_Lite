83fe8323fec890cb40e2a567a0c458ee60310967
AhmadHassan1102 <ahmad.alihussain289@gmail.com>
2022-08-08 17:20:02 +0500

    Login System

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,35 @@
+class UserController < ApplicationController
+  def index   # when click on log in button
+
+
+  end
+  def verifyuser
+    @user = User.find_by(email: params[:user][:email], password: params[:user][:password])
+    if @user
+      session[:id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to "/login"
+    end
+    
+  end
+
+  def login   # login page
+    @user = User.new
+  end
+  
+  def show
+
+    if session[:id]
+      @user =User.find(params[:id])
+    else
+      redirect_to "/login"
+    end
+  end
+
+  def destroy
+    debugger
+    session[:id] = nil     
+    redirect_to '/login' 
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'user/show'
+  get 'user/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
+   get '/login', to: 'user#login'
+   get '/logout', to: 'user#destroy'
+   post '/login', to: 'user#verifyuser'
+   resources :user
+
+
 end
